020e8b70fb68f18b7f8520f28071fb6cbe3d5deb
Linda LEMOFOUET <linda@example.com>
2026-06-21 07:16:38 +0200

    ci: separate e2e tests and firestore rules tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  testIgnore: process.env.FIRESTORE_EMULATOR_HOST ? undefined : '**/security/**',
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
